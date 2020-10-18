ba3888318b4d40d508d399272c71a01daa0fb19e
Navneet <navneetjh5@gmail.com>
2020-10-18 20:11:02 +0530

    ADDING: Peak and Pop function

--- src/main/java/com/cg/datastructures/StackLinkedList.java
+++ src/main/java/com/cg/datastructures/StackLinkedList.java
@@ -1,12 +1,16 @@
 package com.cg.datastructures;
 
+
 public class StackLinkedList {
 
     public static void main(String[] args) {
         LinkedList.Node head = new LinkedList.Node(70);
 
-        displayList(push(head, 30));
-        displayList(push(push(head, 30), 56));
+        head = push(head, 30);
+        displayList(head);
+        head = push(head, 56);
+        displayList(head);
+        peakAndPop(head);
     }
 
     public static LinkedList.Node push(LinkedList.Node head, int data) {
@@ -21,6 +25,16 @@ public class StackLinkedList {
         }
         System.out.println(current);
 
+    }
+
+    public static void peakAndPop(LinkedList.Node head) {
+        LinkedList.Node current = head;
+        while (current != null) {
+            System.out.println("Head is at:" + current.data);
+            LinkedList.Node temp = current;
+            current = current.next;
+            displayList(current);
+        }
     }
 
 
